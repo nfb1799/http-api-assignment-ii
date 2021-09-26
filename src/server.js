bd2f753cd1541451a04894d64ee3c139e87198cb
nfb1799 <nfb1799@rit.edu>
2021-09-26 18:30:56 -0400

    Assignment Complete

--- src/server.js
+++ src/server.js
@@ -5,37 +5,36 @@ const responseHandler = require('./responses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-
 const handlePost = (request, response, parsedURL) => {
-    if(parsedURL.pathname === '/addUser') {
-        const body = [];
+  if (parsedURL.pathname === '/addUser') {
+    const body = [];
 
-        //if there is ever an error, these lines get called
-        request.on('error', (err) => {
-            console.dir(err);
-            response.statusCode = 400;
-            response.end();
-        });
+    // if there is ever an error, these lines get called
+    request.on('error', (err) => {
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
 
-        request.on('data', (chunk) => {
-            body.push(chunk);
-        });
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
 
-        request.on('end', () => {
-            const bodyString = Buffer.concat(body).toString();
-            const bodyParams = query.parse(bodyString);
+    request.on('end', () => {
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
 
-            responseHandler.addUser(request, response, bodyParams);
-        });
-    }
-};  
+      responseHandler.addUser(request, response, bodyParams);
+    });
+  }
+};
 
 const handleGet = (request, response, parsedURL) => {
-    if(parsedURL.pathname === '/') responseHandler.getIndex(request, response);
-    else if(parsedURL.pathname === '/style.css') responseHandler.getStyle(request, response);
-    else if(parsedURL.pathname === '/getUsers') responseHandler.getUsers(request, response);
-    else if(parsedURL.pathname === '/notReal') responseHandler.notReal(request, response);
-    else responseHandler.notFound(request, response);
+  if (parsedURL.pathname === '/') responseHandler.getIndex(request, response);
+  else if (parsedURL.pathname === '/style.css') responseHandler.getStyle(request, response);
+  else if (parsedURL.pathname === '/getUsers') responseHandler.getUsers(request, response);
+  else if (parsedURL.pathname === '/notReal') responseHandler.notReal(request, response);
+  else responseHandler.notFound(request, response);
 };
 
 const onRequest = (request, response) => {
